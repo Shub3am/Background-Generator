de5cb5a222626384d2c320cc6a7523ab04300bde
Shubh3m <Shubhamvishwakarma0604@gmail.com>
2022-03-21 13:02:57 +0530

    Added Current Color

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let body = document.querySelector("body");
 let color_1 = document.querySelector(".color-1");
 let color_2 = document.querySelector(".color-2");
+let CurrentStat = document.querySelector(".stat");
 
 function changeColor() {
-	body.style.background = `linear-gradient(to right, ${color_1.value}, ${color_2.value})`
+  body.style.background = `linear-gradient(to right, ${color_1.value}, ${color_2.value})`;
+  CurrentStat.textContent = `Current Color: ${color_1.value} and ${color_2.value}`;
 }
 
-color_1.addEventListener('input', changeColor);
-color_2.addEventListener('input', changeColor);
+color_1.addEventListener("input", changeColor);
+color_2.addEventListener("input", changeColor);
